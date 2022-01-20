d1d54bf48c07c04f1b5864d81c43310a024318cd
LukyLucc <luccafechner@gmx.de>
2022-01-20 14:52:37 +0100

    V.0.9

--- Plotter.py
+++ Plotter.py
@@ -2,15 +2,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def toArray(dict):
-    x = np.zeros(len(dict))
-    y = np.zeros(len(dict))
-    for i, date in enumerate(dict):
-        x[i] = date
-        y[i] = dict[date]
+def toArray(x,y):
+
     plt.bar(x, y)
     plt.show()
-    return x, y
 
 #xpoints = np.array([0, 6])
 #ypoints = np.array([100, 250])
--- Wrapper.py
+++ Wrapper.py
@@ -1,6 +1,6 @@
 from riotwatcher import *
 from datetime import datetime
-from collections import defaultdict
+import numpy as np
 
 
 class Wrapper:
@@ -31,6 +31,9 @@ class Wrapper:
 
         return matches
 
+    def getSmallMatchHistory(self):
+        pass
+
     def getSummoner(self):
         """
         Methode to get a Specific Summoner
@@ -50,26 +53,35 @@ class Wrapper:
         Methode to get the Date when a Game was played
         :return: Date Object YYYY-MM-DD
         """
-        game = self.watcher.match.by_id("EUROPE", game_id)
+        try:
+            game = self.watcher.match.by_id("EUROPE", game_id)
+        except:
+            return datetime.now().date()
         unix = game['info']['gameCreation']
         unix = int(unix / 1000)
 
-        date = datetime.fromtimestamp(unix)
+        date = datetime.fromtimestamp(unix).date()
         return date
 
-    def getAllDates(self) -> list:
+    def getAllDates(self, matches) -> np.array:
         """
 
         :return: list of datetime
         """
-        #matches = self.getMatchHistory()
-        sampel_matches = ['EUW1_5675455489', 'EUW1_5675378816', 'EUW1_5675402656', 'EUW1_5673282942', 'EUW1_5669648539',
-                          'EUW1_5669620635', 'EUW1_5667456956', 'EUW1_5665085560', 'EUW1_5665080642', 'EUW1_5663575630',
-                          'EUW1_5663433898'
-                          ]
-        dict = defaultdict(int)
+        # matches = self.getMatchHistory()
+        # sampel_matches = ['EUW1_5675455489', 'EUW1_5675378816', 'EUW1_5675402656', 'EUW1_5673282942', 'EUW1_5669648539',
+        #                  'EUW1_5669620635', 'EUW1_5667456956', 'EUW1_5665085560', 'EUW1_5665080642', 'EUW1_5663575630',
+        #                  'EUW1_5663433898'
+        #                  ]
+        x = np.array([], dtype='datetime64')
+        y = np.array([])
 
-        for game_id in sampel_matches:
+        for game_id in matches:
             date = self.getDate(game_id)
-            dict[date.strftime("%d")] += 1
-        return dict
+            if date in x:
+                index = np.where(x == date)
+                y[index] += 1
+            else:
+                x = np.append(x, date)
+                y = np.append(y, 1)
+        return x, y
